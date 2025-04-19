0d34f43bee2a5fa42811adaf0de03e1a3dd207a9
tris203 <admin@snappeh.com>
2025-04-19 13:46:54 +0100

    fixup

--- lua/copilot-lsp/completion/init.lua
+++ lua/copilot-lsp/completion/init.lua
@@ -1,26 +1,26 @@
 local M = {}
 
----@param results table<integer, { err: lsp.ResponseError, result: lsp.InlineCompletionList}>
+---@param _results table<integer, { err: lsp.ResponseError, result: lsp.InlineCompletionList}>
 ---@param _ctx lsp.HandlerContext
 ---@param _config table
-local function handle_inlineCompletion_response(results, _ctx, _config)
-    -- Filter errors from results
-    local results1 = {} --- @type table<integer,lsp.InlineCompletionList>
-
-    for client_id, resp in pairs(results) do
-        local err, result = resp.err, resp.result
-        if err then
-            vim.lsp.log.error(err.code, err.message)
-        elseif result then
-            results1[client_id] = result
-        end
-    end
-
-    for _, result in pairs(results1) do
-        --TODO: Ghost text for completions
-        -- This is where we show the completion results
-        -- However, the LSP being named "copilot" is enough for blink-cmp to show the completion
-    end
+local function handle_inlineCompletion_response(_results, _ctx, _config)
+    -- -- Filter errors from results
+    -- local results1 = {} --- @type table<integer,lsp.InlineCompletionList>
+    --
+    -- for client_id, resp in pairs(results) do
+    --     local err, result = resp.err, resp.result
+    --     if err then
+    --         vim.lsp.log.error(err.code, err.message)
+    --     elseif result then
+    --         results1[client_id] = result
+    --     end
+    -- end
+    --
+    -- for _, result in pairs(results1) do
+    --     --TODO: Ghost text for completions
+    --     -- This is where we show the completion results
+    --     -- However, the LSP being named "copilot" is enough for blink-cmp to show the completion
+    -- end
 end
 
 ---@param type lsp.InlineCompletionTriggerKind
